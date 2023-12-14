5e30ad0c74506c799687f5df31ba18de58cb61ea
ConfidenceYobo <confiyobo@gmail.com>
2023-12-14 14:52:34 +0100

    update hardhat config with network and deployment configurations

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,51 @@
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
 import { HardhatUserConfig } from "hardhat/config";
+import type { NetworkUserConfig } from "hardhat/types";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-gas-reporter"
 
+const bscTestnet: NetworkUserConfig = {
+  url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+  chainId: 97,
+  accounts: [process.env.HARDHAT_TESTNET_KEY!]
+};
+
+const bscMainnet: NetworkUserConfig = {
+  url: "https://bsc-dataseed.binance.org/",
+  chainId: 56,
+  accounts: [process.env.HARDHAT_MAINNET_KEY!],
+};
+
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    testnet: bscTestnet,
+    mainnet: bscMainnet,
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    ],
+  },
   gasReporter: {
     enabled: true
-  }
-
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://bscscan.com/
+    apiKey: process.env.HARDHAT_BSCSCAN_API_KEY
+  },
 };
 
 export default config;
